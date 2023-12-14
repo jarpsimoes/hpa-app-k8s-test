dd943261f813326ff0d695a3ac7ba1456c60b165
João Simões <jarpsimoes@gmail.com>
2023-12-14 17:06:13 +0000

    ADD: more service type

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const app = express();
-
+const winston = require("winston");
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console()
+  ],
+});
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // Usage:
-app.get('/lowservice', async (req, res) => {
+app.get('/service', async (req, res) => {
 	await sleep(2000); // Sleep for 2 seconds
-	res.send('Hello Low Service!');
+	logger.info("service called /service");
+	res.setHeader('Content-Type', 'application/json');
+	res.status(200).send(JSON.stringify({ status: 'OK', time: "static 2000" }));
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/highservice/:time', async (req, res) => {
+	await sleep(req.params.time); // Sleep for 2 seconds
+	logger.info("service called /highservice/" + req.params.time);
+	res.setHeader('Content-Type', 'application/json');
+	res.status(200).send(JSON.stringify({ status: 'OK', time: req.params.time }));
+});
+
+app.get('/health', (req, res) => {
+	logger.info("service called /health");
+	res.setHeader('Content-Type', 'application/json');
+	res.status(200).send(JSON.stringify({ status: 'UP' }));
 });
 
 
